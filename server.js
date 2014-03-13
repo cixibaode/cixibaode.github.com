0fbaa0fd1ec1f94175faafd375b9718d9bf92971
继风 <jifeng.zjd@taobao.com>
2014-03-13 17:34:11 +0800

    nodejs server

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var http = require('http');
-var connet = require('connect');
+var connect = require('connect');
+var fs = require('fs');
+var path = require('path');
+
+var file = path.join(__dirname, './index.html');
+var content = fs.readFileSync(file);
 
 var app = connect();
-app.use(connect.static(__dirname));
+app.use('/assets', connect.static( __dirname));
+app.use(function (req, res, next) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(content);
+});
 
 var server = http.createServer(app);
 
 server.listen(8080, function () {
   console.log('Server running at http://127.0.0.1:80/');
 });
-
-
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\n');
-// }).listen(80, '127.0.0.1');
-// console.log('Server running at http://127.0.0.1:80/');
